b3a482ee6331b52fb962c90c2612e62312741c5f
vaceslavkameronov <slka@mail.ru>
2023-05-22 20:46:39 +0400

    Создание проекта ДЗ по теме "Spring и Mockito"

---
+++ src/main/java/com/prosky/homeworkspringmockito/HomeWorkSpringMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.prosky.homeworkspringmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpringMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWorkSpringMockitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prosky/homeworkspringmockito/HomeWorkSpringMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosky.homeworkspringmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWorkSpringMockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
